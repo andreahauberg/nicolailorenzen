e207590bce04d6b10758801f726a31df15023148
Andrea <132267442+andreahauberg@users.noreply.github.com>
2023-12-14 11:05:01 +0100

    finish v4

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,12 +1,12 @@
 window.onscroll = function() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      document.querySelector("scroll-to-top").style.display = "block";
+      document.getElementById("scroll-to-top").style.display = "block";
     } else {
-      document.querySelector("scroll-to-top").style.display = "none";
+      document.getElementById("scroll-to-top").style.display = "none";
     }
   };
   
-  document.querySelector("scroll-to-top").addEventListener("click", scrollToTop);
+  document.getElementById("scroll-to-top").addEventListener("click", scrollToTop);
 
   function scrollToTop() {
     document.body.scrollTop = 0;
